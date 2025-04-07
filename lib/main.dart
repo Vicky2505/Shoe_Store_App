db9d25c00ce4880064089598a3a0648504774b0d
Vignesh <154775436+Vicky2505@users.noreply.github.com>
2025-04-07 15:23:24 +0530

    responsiveness for all mobile screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:sneakers/screens/bottom_nav_bar.dart';
 
 void main() {
@@ -7,15 +8,24 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Shoe Store App',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: BottomNavBar(),
-      debugShowCheckedModeBanner: false,
+    return ScreenUtilInit(
+      designSize: const Size(360,640),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) {
+        return MaterialApp(
+          title: 'Sneakers App',
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+            textTheme: Typography.englishLike2018.apply(fontSizeFactor: 1.sp),
+          ),
+          debugShowCheckedModeBanner: false,
+          home: const BottomNavBar(),
+        );
+      },
     );
   }
 }
