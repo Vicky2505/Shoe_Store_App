c830c112aea48ff60064fbe3ddd44676fc0e53a7
Vignesh <154775436+Vicky2505@users.noreply.github.com>
2025-04-08 12:20:09 +0530

    complete ui of bottom nav in responsive

--- lib/screens/bottom_nav_bar.dart
+++ lib/screens/bottom_nav_bar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:sneakers/screens/favorite_screen/favorite_screen.dart';
 import 'package:sneakers/screens/home_screen/home_screen.dart';
 import 'package:sneakers/screens/notification_screen/notification_screen.dart';
@@ -61,11 +62,11 @@ class _BottomNavBarState extends State<BottomNavBar> {
                 _buildIcon(Icons.shopping_bag_outlined, 3),
                 if (_selectedIndex != 3)
                   Positioned(
-                    bottom: 34,
-                    right: 8,
+                    bottom: 34.h,
+                    right: 8.w,
                     child: Container(
-                      width: 8,
-                      height: 8,
+                      width: 8.w,
+                      height: 8.w,
                       decoration: BoxDecoration(
                         color: Colors.redAccent,
                         shape: BoxShape.circle,
@@ -85,8 +86,8 @@ class _BottomNavBarState extends State<BottomNavBar> {
     bool isSelected = _selectedIndex == index;
 
     return Container(
-      height: 50,
-      width: 50,
+      height: 50.w,
+      width: 50.w,
       decoration:
           isSelected
               ? BoxDecoration(color: Colors.black87, shape: BoxShape.circle)
@@ -95,7 +96,7 @@ class _BottomNavBarState extends State<BottomNavBar> {
         icon,
         // ignore: deprecated_member_use
         color: isSelected ? Colors.white : Colors.black.withOpacity(0.5),
-        size: 26,
+        size: 26.sp,
       ),
     );
   }
